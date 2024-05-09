6f99b5db718ff7403c525f1e0eafd9626ad13922
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-05-09 17:50:13 +0100

    getting the data from the db and passing it to the dashboardcard conponents

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,12 +5,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import db from "@/db/db";
 import React from "react";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+export default async function AdminDashboard() {
+  const { amount, numberOfSales } = await getSalesData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="TITLE 1" subtitle="SUBTITLE 1" body="BODY 1" />
+      <DashboardCard title="TITLE 1" subtitle={numberOfSales} body={amount} />
       <DashboardCard title="TITLE 2" subtitle="SUBTITLE 2" body="BODY 2" />
       <DashboardCard title="TITLE 3" subtitle="SUBTITLE 3" body="BODY 3" />
     </div>
