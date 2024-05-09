77f5a41a5690888a198290d16e42b8a1e0347a96
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-05-09 17:28:51 +0100

    creating card with shadcn and making it dynamic by accepting props instead of raw data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 export default function AdminDashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="TITLE 1" subtitle="SUBTITLE 1" body="BODY 1" />
+      <DashboardCard title="TITLE 2" subtitle="SUBTITLE 2" body="BODY 2" />
+      <DashboardCard title="TITLE 3" subtitle="SUBTITLE 3" body="BODY 3" />
+    </div>
+  );
+}
+
+type DashboardCardTypes = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardTypes) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ import React, { ComponentProps } from "react";
 
 export function Nav({ children }: { children: React.ReactNode }) {
   return (
-    <nav className="bg-stone-800 text-white flex justify-center px-4">
+    <nav className="bg-slate-600 mt-2 rounded-2xl md:w-2/3 xl:w-1/2  mx-auto text-white flex space-x-1 justify-center px-4">
       {children}
     </nav>
   );
@@ -19,8 +19,9 @@ export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
     <Link
       {...props}
       className={cn(
-        "p-3 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
-        pathname === props.href && "bg-background text-foreground"
+        "p-3 hover:bg-secondary hover:text-secondary-foreground hover:rounded-2xl hover:border-y-2  hover:border-slate-600  focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:rounded-2xl focus-visible:border-y-2  focus-visible:border-slate-600",
+        pathname === props.href &&
+          "bg-background rounded-2xl border-y-2 border-slate-600 text-slate-600 "
       )}
     />
   );
